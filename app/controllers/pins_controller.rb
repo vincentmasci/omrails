2051507ae19eba3c3d2c76141cb3f80e7b1f6211
Vince Masci <vincentmasci@iinet.net.au>
2013-08-14 07:04:25 +1000

    Add JQuery Masonry

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all #current_user.pins.all (just for user pins)
+    @pins = Pin.order("created_at desc") #current_user.pins.all (just for user pins)
 
     respond_to do |format|
       format.html # index.html.erb
